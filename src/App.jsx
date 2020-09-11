8f07fd80f4f58ebf4c1b3899c267afd8364c3647
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2020-09-11 16:08:01 -0300

    ADD: connect graphql

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./App.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 import Home from "./view/home";
+
+const client = new ApolloClient({
+    uri: "https://48p1r2roz4.sse.codesandbox.io",
+    cache: new InMemoryCache(),
+});
 
 function App() {
     return (
-        <div className="App">
-            <Home />
-        </div>
+        <ApolloProvider client={client}>
+            <div className="App">
+                <Home />
+            </div>
+        </ApolloProvider>
     );
 }
 
