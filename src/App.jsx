ebb9d6c4e63561824582d1790ceb0b68dc0a166a
rodrigogomezcerda <rodrigo.gomez.cerda@gmail.com>
2020-10-06 15:13:34 -0300

    ADD: env file

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Home from "./view/home";
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000",
+    uri: process.env.REACT_APP_API_URL,
     cache: new InMemoryCache(),
 });
 
