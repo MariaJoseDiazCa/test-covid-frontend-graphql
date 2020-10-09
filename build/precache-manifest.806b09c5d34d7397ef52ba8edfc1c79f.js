048c79ddb51ef65760a9bacafb1dfe31e800cf48
rodrigogomezcerda <rodrigo.gomez.cerda@gmail.com>
2020-10-09 14:29:12 -0300

    ADD: build

---
+++ build/precache-manifest.806b09c5d34d7397ef52ba8edfc1c79f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a78b9c693ba8929882be3f9ab9a904b",
+    "url": "/test-covid-frontend-api/index.html"
+  },
+  {
+    "revision": "2f16c748a83ca5e22efa",
+    "url": "/test-covid-frontend-api/static/css/main.a32d548d.chunk.css"
+  },
+  {
+    "revision": "80cae500e0c41743ab93",
+    "url": "/test-covid-frontend-api/static/js/2.ec85838f.chunk.js"
+  },
+  {
+    "revision": "7baac2e68c562348b516173f26c107b7",
+    "url": "/test-covid-frontend-api/static/js/2.ec85838f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f16c748a83ca5e22efa",
+    "url": "/test-covid-frontend-api/static/js/main.209f09c4.chunk.js"
+  },
+  {
+    "revision": "2fc8d7642cab5371e66a",
+    "url": "/test-covid-frontend-api/static/js/runtime-main.d467b001.js"
+  },
+  {
+    "revision": "55a698fa94e1a713fa8f54112ae65fac",
+    "url": "/test-covid-frontend-api/static/media/social_distancing.55a698fa.png"
+  }
+]);
