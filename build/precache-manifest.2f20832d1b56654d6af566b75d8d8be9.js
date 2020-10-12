8bb187e18b87473a44977cc73bea82908ca079c1
rodrigogomezcerda <rodrigo.gomez.cerda@gmail.com>
2020-10-12 10:02:32 -0300

    UPDATE: build and package.json

---
+++ build/precache-manifest.2f20832d1b56654d6af566b75d8d8be9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c451bdde2d518ac536e010b9caef48c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94cc8a6df1b1f7eea190",
+    "url": "/static/css/main.efc36cbf.chunk.css"
+  },
+  {
+    "revision": "80cae500e0c41743ab93",
+    "url": "/static/js/2.ec85838f.chunk.js"
+  },
+  {
+    "revision": "7baac2e68c562348b516173f26c107b7",
+    "url": "/static/js/2.ec85838f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cc8a6df1b1f7eea190",
+    "url": "/static/js/main.79b4e1bd.chunk.js"
+  },
+  {
+    "revision": "588d3bd80df6752531ad",
+    "url": "/static/js/runtime-main.531f709b.js"
+  },
+  {
+    "revision": "55a698fa94e1a713fa8f54112ae65fac",
+    "url": "/static/media/social_distancing.55a698fa.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-covid-frontend-api/precache-manifest.806b09c5d34d7397ef52ba8edfc1c79f.js"
+  "/precache-manifest.2f20832d1b56654d6af566b75d8d8be9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-covid-frontend-api/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
